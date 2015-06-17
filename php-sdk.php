303bd4c428efee6461744c1aa7219f2543eca2dd
app@miniyun.cn <app@miniyun.cn>
2015-06-17 11:44:11 +0800

    v21

--- php-sdk.php
+++ php-sdk.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * 迷你云PHP-SDK
- * 适用迷你云1.5版本，暂时不支持1.6+版本
- */
-class MiniSDK15{	
+ * 适用迷你云2.1版本
+ */ 
+require_once("./HttpClient.php");
+class MiniSDK21{	
 	//appKey，由管理事先分配好
 	private $appKey="d6n6Hy8CtSFEVqNh";
 	//appSecret，由管理事先分配好
@@ -12,10 +13,14 @@ class MiniSDK15{
 	private $accessToken;
 	//迷你云服务器Host
 	private $host; 
+	//上传文件的大小
+	private $fileSize;
+	//上传文件的signature
+	private $fileSignature;
 	/**
 	 * 构造函数
 	 */
-	public function MiniSDK15($host){
+	public function MiniSDK21($host){
 		$this->host = $host;
 	}
 	/**
@@ -33,7 +38,7 @@ class MiniSDK15{
 					'client_id'=>$this->appKey,
 					'client_secret'=>$this->appSecret,
 				); 		
-		$result = json_decode($this->request($url,$data));		
+		$result = json_decode($this->request($url,$data)); 	 
 		$this->accessToken = $result->{"access_token"}; 
 		return $result;
 	}
@@ -74,37 +79,73 @@ class MiniSDK15{
 		return $result;
 	}
 	/**
+	* 迷你存储秒传接口
+	*/
+	private function miniStoreFileSec($secInfo){
+		$hashCode = $this->fileSignature; 
+		$fileSize = $this->fileSize;
+		//获得迷你存储子节点地址
+		$miniStoreHost = $secInfo->{"url"};
+		//回调地址，客户端只需传递该值即可
+		$callback = $secInfo->{"callback"};  
+		$params = array(
+				"route"=>"file/sec", 
+				"signature"=>$hashCode,
+				"size"=>$fileSize,
+				"callback"=>$callback
+		); 
+		$http = new HttpClient(); 
+		$http->post($miniStoreHost,$params); 
+		$body = $http->get_body();
+		$result = json_decode($http->get_body());
+		return $result;
+	}
+	/**
+	* 迷你存储文件上传接口
+	*/
+	private function miniStoreFileUpload($localPath,$secInfo){ 
+		$hashCode = $this->fileSignature; 
+		$fileSize = $this->fileSize;
+		$callback = $secInfo->{"callback"};  
+		//TODO 这里可根据断点位置读取内容上传
+		$params = array(
+			"route"=>"file/upload", 
+			"signature"=>$hashCode,
+			"size"=>$fileSize,
+			"callback"=>$callback,
+			"offset"=>0//offset这里强制从0开始
+		);  
+		$http = new HttpClient(); 
+		$files = array( 
+			'files'=>$localPath,//文件的头标记是files
+		);
+		$http->post($secInfo->{"url"},$params,$files);
+		$result = json_decode($http->get_body());
+		return $result;
+	}
+	/**
 	 * 创建文件
 	 */
 	public function createFile($localPath,$remotePath){ 
-		$siteInfo = $this->siteInfo();
-		if(array_key_exists("dataMode",$siteInfo)){
-			$dataMode = $siteInfo->{"dataMode"}; 
-			if($dataMode->{"source"}==="miniStor"){
-				//启用迷你存储
-			    $data = $this->miniStoreSec($localPath,$remotePath); 
-				//如果返回值uri属性标记该文件上传的迷你存储的地址,说明该文件需要上传
-				//如果没有uri,否则标记文件已经上传成功了
-				if(array_key_exists("uri",$data)){
-					//该文件需要上传
-					//与文件二进制内容一并提交的，还有秒传接口返回的reqeust子对象，将其序列化到url地址
-					//从秒传接口返回的uri作为迷你存储的入口地址
-					$url    = $data->{"uri"}."?";
-					$params = $data->{"request"};
-					foreach($params as $key=>$value) { $url .= $key.'='.$value.'&'; }  
-					include "./HttpClient.php";
-					$http = new HttpClient(); 
-					$files = array( 
-						'files'=>$localPath,//文件的头标记是files
-					); 
-					$http->post($url,array(),$files); //提交文件内容的时候，网络不稳定可能会出现错误，通过返回值进行处理
-					return json_decode($http->get_body());
-				}else{
-					return $data;//该文件秒传成功
+		//先获得文件的signature以及size
+		$this->fileSignature = $this->hashFile($localPath); 
+		$this->fileSize = filesize($localPath);
+
+		$secInfo = $this->fileSec($remotePath);
+		if(!$secInfo->{"success"}){
+			//文件秒传没有成功，需要上传该文件
+			$storeType = $secInfo->{"store_type"};
+			if($storeType=="miniStore"){
+				//上传文件之前，先获得该文件是否是断点文件，便于文件断点续传
+				$storeSecInfo = $this->miniStoreFileSec($secInfo); 
+				if(!$storeSecInfo->{"success"}){
+					$result = $this->miniStoreFileUpload($localPath,$secInfo);
+					return $result;
 				}
-				
-			} 
-		}				 
+			}			
+		}else{
+			return $secInfo;
+		}  			 
 	}
 	/**
 	 * 获得文件下载地址
@@ -129,18 +170,19 @@ class MiniSDK15{
 		return $result;
 	}
 	/**
-	 * 迷你存储秒传接口
+	 * 储秒传接口
 	 * 如果服务器已经存储有该文件内容，则直接成功
 	 * 如果服务器没有存储文件内容，在返回迷你存储服务器地址
 	 */
-	public function miniStoreSec($localPath,$remotePath){
-		$hashCode = $this->hashFile($localPath); 
-		$fileSize = filesize($localPath);
+	public function fileSec($remotePath){
+		$hashCode = $this->fileSignature; 
+		$fileSize = $this->fileSize;
 		
-		$url = $this->host."/api.php/1/paramsdata/miniyun".urlencode($remotePath);
-		$data = array( 
+		$url = $this->host."/api.php";
+		$data = array(
+			"route"=>"file/sec",
 			"path"=>$remotePath,
-			"hash"=>$hashCode,
+			"signature"=>$hashCode,
 			"size"=>$fileSize,
 			"offset"=>0,
 			"overwrite"=>true,
@@ -186,18 +228,6 @@ class MiniSDK15{
 			"root"=>"miniyun",
 			"path"=>$path,	 
 			);
-		$data = $this->signUrl2Paramters($url,$data);  		
-		$result = json_decode($this->request($url,$data));
-		return $result;
-	}
-	/**
-	 * 创建外链，适合文件夹/文件
-	 */
-	public function createLink($path){ 
-		$url = $this->host."/api.php/1/link/create".urlencode($path);
-		$data = array(
-			"root"=>"miniyun", 
-			); 
 		$data = $this->signUrl2Paramters($url,$data);  		
 		$result = json_decode($this->request($url,$data));
 		return $result;
@@ -277,39 +307,36 @@ class MiniSDK15{
 	}
 }
 //迷你云服务器地址
-$host = "http://yp.nje.cn";
+$host = "http://pan.nje.cn";
 //用户名
-$userName = "jktest1";
+$userName = "ceshid";
 //用户密码
-$userPassword = "jktest@1";
-$miniSDK = new MiniSDK15($host);
+$userPassword = "123456@nje.cn";
+$miniSDK = new MiniSDK21($host);
 $data = $miniSDK->login($userName,$userPassword); 
-print_r($data);
+// print_r($data);
 //文件列表
-//$data = $miniSDK->listFile("/");
-//print_r($data);
+// $data = $miniSDK->listFile("/7/");
+// print_r($data);
 //获得用户信息
-//$data = $miniSDK->accountInfo();
-//print_r($data);
+// $data = $miniSDK->accountInfo();
+// print_r($data);
 //创建目录
-//$data = $miniSDK->createFolder("/测试1/测试2/测试31");
-//print_r($data);
+// $data = $miniSDK->createFolder("/7/测试1/测试2/测试31");
+// print_r($data);
 //修改名称
-//$data = $miniSDK->rename("/测试1/测试2/测试3","/测试1/测试2/测试4");
-//print_r($data);
-//修改名称
-//$data = $miniSDK->delete("/测试1/测试2/测试4");
-//print_r($data);
-//创建外链
-//$data = $miniSDK->createLink("/测试1/测试2");
-//print_r($data);
+// $data = $miniSDK->rename("/7/测试1/测试2/测试311","/7/测试1/测试2/测试4");
+// print_r($data);
+//删除目录
+// $data = $miniSDK->delete("/7/测试1/测试2/测试4");
+// print_r($data);
 //获得迷你云站点信息
 //$data = $miniSDK->siteInfo();
 //print_r($data);
 //上传文件
 //请注意，第二个参数是迷你云服务器绝对路径
-//$data = $miniSDK->createFile("c:/test.txt","/测试1/test3.txt");
-//print_r($data);
+// $data = $miniSDK->createFile("/root/test.txt","/7/测试1/test3.txt");
+// print_r($data);
 //下载文件 
-//$data = $miniSDK->getDownloadUrl("/测试1/test3.txt");
-//print_r($data);
+// $data = $miniSDK->getDownloadUrl("/7/测试1/test3.txt");
+// print_r($data);
